952a8c526009e2ea391a411a4916d131cca0752c
catuxaseoane <82618881+catuxaseoane@users.noreply.github.com>
2023-04-12 20:48:03 +0200

    Add files via upload

--- src/bonus1.py
+++ src/bonus1.py
@@ -3,7 +3,10 @@ Challenge Catuxa Seoane Botana - Magine Pro interview
 12/04/2023
 
 
-Build a simple Python script that processes a video file and uploads it to an S3 bucket.
+Bonus Assignment 1:
+Extend the Python script to process a folder of video files instead of a single video file.
+The script should iterate through all files in the folder and process and upload each file
+to the specified S3 bucket
 
 '''
 
@@ -45,13 +48,15 @@ args = parser.parse_args()
 #INPUT FILE PATH
 in_file_path = args.input
 # Check if input file path is correct
-while path.isfile(in_file_path) == False:
-    print("I did not find the file at "+str(in_file_path)+'. Please check the directory path and try again.: ')
+while path.exists(in_file_path) == False:
+    print("The directory "+str(in_file_path)+' is incorrect. Please check the directory path and try again.: ')
     in_file_path = input()
 
 #OUTPUT FILE PATH
 out_file_path = args.output
-
+while path.exists(out_file_path) == False:
+    print("The directory "+str(out_file_path)+' is incorrect. Please check the directory path and try again.: ')
+    in_file_path = input()
 #S3 bucket
 s3_bucket = args.bucket
 #S3 object key
@@ -68,11 +73,19 @@ def video_format(in_file_path, out_file_path):
     :param out_file_path: Output file name
     :return: Clip ready to upload
     """
+    count=0
     print('Changing format and resizing...')
-    clip=moviepy.VideoFileClip(in_file_path)
-    #resizing
-    clip.resize((300,300))
-    clip.write_videofile(out_file_path,codec='libx264')
+    for file in os.scandir(in_file_path):
+        count=count+1
+        filename=os.fsdecode(file)
+        
+        print('Processing the file'+str(filename))
+        
+        clip=moviepy.VideoFileClip(filename)
+        #resizing
+        clip.resize((300,300))
+        print((str(out_file_path)+'/'+str(filename)+".mov"))
+        clip.write_videofile(str(out_file_path)+'/'+'newvideo'+str(count)+".mov",codec='libx264')
     
 
 
@@ -84,38 +97,37 @@ def upload_file(out_file_path, s3_bucket, s3_object_key=None):
     :param s3_object_key: S3 object name. If not specified then clip name is used
     :return: True if file was uploaded, else False
     """
-    
-
-    # If S3 object_name was not specified, use file_name
-    if s3_object_key is None:
-        s3_object_key = os.path.basename(out_file_path)
-    
-    
-    #Ask for credentials
+        #Ask for credentials
     print("Please write your AWS ACCESS KEY ID: ")
     aws_access_key_id = input()
     print("Please write your AWS SECRET ACCESS KEY: ")
     aws_secret_access_key=input()
-    
 
-    # Upload the file
-    s3_client = boto3.client('s3',aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-    try:
-        response = s3_client.upload_file(out_file_path, s3_bucket, s3_object_key)
-    except ClientError as e:
-        print('Client error : ')
-        print(e)
-        logging.error(e)
-        return False
-    print('The video has been uploaded to the platform successfully!')
-    return True
+
+    count=0
+    for file in os.scandir(out_file_path):
+        count=count+1
+        print(count)
+        filename=os.fsdecode(file)
+        print('Uploading the file'+str(filename))
+        # If S3 object_name was not specified, use file_name
+        if s3_object_key is None:
+            s3_object_key = os.path.basename(filename)
+    
+        # Upload the file
+        s3_client = boto3.client('s3',aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        try:
+            response = s3_client.upload_file(filename, s3_bucket, s3_object_key+"_"+str(count)+".mov")
+        except ClientError as e:
+            print('Client error : ')
+            print(e)
+            logging.error(e)
+            
+        print('The video has been uploaded to the platform successfully!')
+        
 
 
 #Calling the functions
 
 video_format(in_file_path, out_file_path)
-while path.isfile(out_file_path) == False:
-    print("The video could not be saved to the directory "+str(out_file_path)+'.Check the parameters and run again. ')
-    break
-
 upload_file(out_file_path, s3_bucket, s3_object_key)
